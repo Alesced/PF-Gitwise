e46268d53134e929ca8e48274e2320fbb62cc0da
Natalie8a <naty.8a@outlook.com>
2025-06-28 00:06:01 +0000

    agregué member_since (pues, traté, vamos a ver si funciona)

--- src/api/models.py
+++ src/api/models.py
@@ -4,18 +4,20 @@ from sqlalchemy.orm import Mapped, mapped_column
 import datetime
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(120), nullable=False)
     last_name: Mapped[str] = mapped_column(String(120), nullable=False)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    gh_login: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    gh_login: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    username: Mapped[str] = mapped_column(
+        String(50), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-    #member_since: Mapped[datetime] = mapped_column(Date#no sé cómo hacer esto)
-
-
+    member_since: Mapped[datetime] = mapped_column(Date(), nullable=False)
 
     def serialize(self):
         return {
@@ -25,8 +27,8 @@ class User(db.Model):
             "email": self.email,
             "gh_login": self.gh_login,
             "username": self.username,
-            "is_active": self.is_active
-            #"member_since": self.member_since
+            "is_active": self.is_active,
+            "member_since": self.member_since,
             # do not serialize the password, its a security breach
         }
         print('hello world')
