dd9a4dd4ea79ec758b8e04daf557cb624d55bd20
Alesced <alesc2413@gmail.com>
2025-07-04 23:39:04 +0000

    falta probar la ruta con postman

--- migrations/versions/3c66536485a2_.py
+++ migrations/versions/3c66536485a2_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 9de9259c2d71
+Revision ID: 3c66536485a2
 Revises: 
-Create Date: 2025-07-03 18:15:56.810471
+Create Date: 2025-07-04 23:28:17.692370
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '9de9259c2d71'
+revision = '3c66536485a2'
 down_revision = None
 branch_labels = None
 depends_on = None
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Post
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 from flask_bcrypt import Bcrypt
@@ -109,14 +109,11 @@ def handle_new_post():
 @api.route('/search-ia', methods=['POST'])
 @jwt_required()
 def handle_search_ia():
-    user_id = get_jwt_identity()
-    data = request.get_json()
-    query = data.get('query')
+    response_body = {
+        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the POST request"
+    }
 
-#here we need to implement the api call to the IA service
-    # For now, we will just return the query as a response
-    if not query:
-        return jsonify({"msg": "Query is required"}), 400
+    return jsonify(response_body), 200
 
 
 
