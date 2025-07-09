6e30995284f71bb5c9db35839975746a78f70234
albertdcm <albertdcm2828@gmail.com>
2025-07-09 18:36:30 +0000

    Resuelto conflicto en Footer.jsx

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -1,14 +1,9 @@
-
 export const Footer = () => {
   return (
     <footer className="bg-dark text-white py-4 mt-auto text-center">
       <div className="container">
         <p className="mb-1">
-<<<<<<< HEAD
-          Final project by <strong>4Geeks</strong>. Built by <strong>Albert, Alejandro, Diego & Nati</strong>
-=======
           Proyecto final <strong>4Geeks</strong>. Elaborado por <strong>Albert, Alejandro, Diego & Natalie</strong>
->>>>>>> 76cf8896d94322b797364f97cf293fc7c1b3263b
         </p>
         <p className="mb-0" style={{ color: "#2563eb" }}>
           © 2025 GitWise
@@ -17,4 +12,3 @@ export const Footer = () => {
     </footer>
   );
 };
-
