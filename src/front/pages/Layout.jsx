d9c853f82273af12d175f34aab8f5cbe82e38236
albertdcm <albertdcm2828@gmail.com>
2025-07-09 22:27:02 +0000

    Conflicto resuelto en Layout.jsx con estructura completa

--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -3,7 +3,6 @@ import ScrollToTop from "../components/ScrollToTop";
 import { Navbar } from "../components/Navbar";
 import { Footer } from "../components/Footer";
 
-<<<<<<< HEAD
 // Base layout component with navbar, footer and scroll-to-top
 export const Layout = () => {
   return (
@@ -18,15 +17,3 @@ export const Layout = () => {
     </div>
   );
 };
-=======
-// Base component that maintains the navbar and footer throughout the page and the scroll to top functionality..
-export const Layout = () => {
-    return (
-        <ScrollToTop>
-            <Navbar />
-                <Outlet />
-            <Footer />
-        </ScrollToTop>
-    )
-}
->>>>>>> f7eab0d18cd1190cf7a6b93fec65f2ed2b275ae6
