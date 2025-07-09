632b712aab18263648e3867ab1f2e78a01e98f29
albertdcm <albertdcm2828@gmail.com>
2025-07-09 18:35:08 +0000

    Resuelto conflicto en routes.jsx

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -7,16 +7,16 @@ import {
 import { Layout } from "./pages/Layout";
 import { Home } from "./pages/Home";
 import { Single } from "./pages/Single";
-import { SinglePost } from "./pages/SinglePost"; 
+import { SinglePost } from "./pages/SinglePost";
 import { Demo } from "./pages/Demo";
 import { Login } from "./pages/Login";
 import { Register } from "./pages/Register";
 import { Posts } from "./pages/Posts";
 import { PostForm } from "./pages/PostForm";
 import { AdminPosts } from "./pages/AdminPosts";
-<<<<<<< HEAD
 import { UserProfile } from "./pages/UserProfile";
 import { AISearch } from "./pages/AISearch";
+import { Contact } from "./pages/Contact";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
@@ -24,27 +24,15 @@ export const router = createBrowserRouter(
       <Route path="/" element={<Home />} />
       <Route path="/single/:theId" element={<Single />} />
       <Route path="/post/:theId" element={<SinglePost />} /> {/* ✅ nueva ruta */}
-=======
-import { Contact } from "./pages/Contact";
-
-export const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Layout />} errorElement={<h1>Not found!</h1>} >
-      <Route path="/" element={<Home />} />
-      <Route path="/single/:theId" element={<Single />} />
->>>>>>> 76cf8896d94322b797364f97cf293fc7c1b3263b
       <Route path="/demo" element={<Demo />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/posts" element={<Posts />} />
       <Route path="/post-form" element={<PostForm />} />
       <Route path="/admin" element={<AdminPosts />} />
-<<<<<<< HEAD
       <Route path="/profile" element={<UserProfile />} />
       <Route path="/AI-search" element={<AISearch />} />
-=======
       <Route path="/contact" element={<Contact />} />
->>>>>>> 76cf8896d94322b797364f97cf293fc7c1b3263b
     </Route>
   )
 );
